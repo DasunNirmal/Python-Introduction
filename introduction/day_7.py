8ac6cf29a5ca8cda68933ea7a9c216a489f0bc1a
DasunNirmal <dasun200301@gmail.com>
2024-12-17 09:33:59 +0530

    numpy

--- introduction/day_7.py
+++ introduction/day_7.py
@@ -1,2 +1,28 @@
 #Python Libraries and Data Processing
 
+# 1. Numpy
+
+import numpy as np
+
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+sumArray = a+b
+subArray = a-b
+mulArray = a*b
+
+
+print("Sum",sumArray)
+print("Sub",subArray)
+print("Mul",mulArray)
+
+
+# Square root
+c = 16
+result_sqrt = np.sqrt(c)
+print("Square of",c,"-",result_sqrt)
+
+# Maximum and Minimum
+result_max = np.max(a)
+result_min = np.min(a)
+print("Maximum of",a,"-",result_max)
+print("Minimum of",a,"-",result_min)
