7da402171d4eed148f0fa038c948b7b5f309bfa5
DasunNirmal <dasun200301@gmail.com>
2024-12-17 11:59:46 +0530

    Array Initializtion

--- introduction/day_7.py
+++ introduction/day_7.py
@@ -39,23 +39,50 @@ array_2 = np.array([[[1,2,5],[2,3,5]], [[3,4,4],[5,8,8]]])
 # print("Data type",array_2.dtype)
 # print("Direct Dimatoin",array_2.ndim)
 
-#Numpy Indexing
+# Numpy Indexing
 # print('2nd Elemt on 1st row',array_1[0,1])
 
 
 # Task
 #1.create a 2d numpy arrray which has daimentions 4,5 which contains the numbers 1 to 20
 array_3 = np.array([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20]])
-print("2D Array:\n", array_3)
+# print("2D Array:\n", array_3)
 
 # Add 10 to every element
 array_add_10 = array_3 + 10
-print("Array after adding 10:\n", array_add_10)
+# print("Array after adding 10:\n", array_add_10)
 
 # Multiply every element by 2
 array_multiply_2 = array_3 * 2
-print("Array after multiplying by 2:\n", array_multiply_2)
+# print("Array after multiplying by 2:\n", array_multiply_2)
 
 # Calculate square of each element
 array_square = np.square(array_3)
-print("Array after squaring each element:\n", array_square)
+# print("Array after squaring each element:\n", array_square)
+
+
+# Boolean indexing in numpy
+array_4 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+# condition = array_4 > 5
+# print(condition)
+# print("Elements greater than 5:", array_4[condition])
+
+filtered_array = array_4[array_4 > 5]
+# print("Filtered Array:", filtered_array)
+
+filtered_combined = array_4[(array_4 > 5) & (array_4 < 10)]
+# print("Filtered Combined Array:", filtered_combined)
+
+
+# Array Initialization
+
+array_5 = np.zeros(4, dtype=int, order='C')
+array_6 = np.zeros((2, 3))
+# print("Array of zeros:", array_5)
+# print("Array of zeros 2D:", array_6)
+
+array_7 = np.full(4, 5)
+array_7 = np.full((2, 3), 5)
+print("Array of full:\n", array_7)
+print("Array of full 2D:\n", array_7)
