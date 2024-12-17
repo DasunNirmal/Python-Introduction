ec1e830e14bfd4b9073fecb925268ba748e96906
DasunNirmal <dasun200301@gmail.com>
2024-12-17 09:40:40 +0530

    2D arrays and 3D arrays with numpy

--- introduction/day_7.py
+++ introduction/day_7.py
@@ -10,19 +10,27 @@ sumArray = a+b
 subArray = a-b
 mulArray = a*b
 
-
-print("Sum",sumArray)
-print("Sub",subArray)
-print("Mul",mulArray)
+# print("Sum",sumArray)
+# print("Sub",subArray)
+# print("Mul",mulArray)
 
 
 # Square root
 c = 16
 result_sqrt = np.sqrt(c)
-print("Square of",c,"-",result_sqrt)
+# print("Square of",c,"-",result_sqrt)
 
 # Maximum and Minimum
 result_max = np.max(a)
 result_min = np.min(a)
-print("Maximum of",a,"-",result_max)
-print("Minimum of",a,"-",result_min)
+# print("Maximum of",a,"-",result_max)
+# print("Minimum of",a,"-",result_min)
+
+# Multidimanional Array
+array_1 = np.array([[1, 2], [3, 4]])
+print("Array 2D",array_1)
+print(array_1.size)
+
+array_2 = np.array([[[5, 6], [7, 8], [9, 10]]])
+print("Array 3D",array_2)
+print(array_2.size)
